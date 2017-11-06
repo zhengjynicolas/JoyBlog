dd20a7f9c45d97f33c2fe2e6d68706d4760eb7b5
zhengjynicolas <zhengjynicolas@hotmail.com>
2017-11-06 22:59:02 +0800

    index page-navi

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,39 @@
+var Initial = false;
 window.onload = function() {
-    particlesJS.load('particles-js', 'json/particles-polygon.json', function() {
-      console.log('callback - particles.js config loaded');
-    });
+    function addListeners() {
+        $("#naviBar").on('click', function(e) {
+            var targetEl = e.target.parentNode;
+            if (targetEl.id != "mine") {
+                if (!$(targetEl).hasClass("active")) {
+                    $(targetEl).addClass("active");
+                    clearSiblingsClass(targetEl, "active");
+                }
+                // loadMainContent(targetEl);
+            }
+        });
+        var clearSiblingsClass = function(el, cls) {
+            var siblings = el.parentNode.children;
+            for (var i = 0; i < siblings.length; i++) {
+                if (el.id === siblings[i].id) {
+                    continue;
+                }
+                if ($(siblings[i]).hasClass(cls)) {
+                    $(siblings[i]).removeClass(cls);
+                }
+            }
+        }
+    }
+
+    function initEverything() {
+	    particlesJS.load('particles-js', 'json/particles-polygon.json', function() {
+	    	console.log("This is Joy's blog page.");
+	    });
+        addListeners();
+        // initMainContent();
+    }
+
+    if (!this.Initial) {
+        initEverything();
+        this.Initial = false;
+    }
 }
