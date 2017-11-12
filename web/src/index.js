221085647209e0e46d2e47ea9a28662e3cef1c40
zhengjynicolas <zhengjynicolas@hotmail.com>
2017-11-12 21:30:30 +0800

    +animation

--- web/src/index.js
+++ web/src/index.js
@@ -1,7 +1,20 @@
 var Initial = false;
 window.onload = function() {
-    function addListeners() {
-        $("#naviBar").on('click', function(e) {
+    var bgColorIdx;
+    var colorList;
+
+    function initGlobalVars() {
+        bgColorIdx = 0;
+        colorList = ['#FF4040', '#FF9840', '#FFF273', '#74E868', '#36BBCE', '#514ED9', '#8E41D5'];
+    }
+
+    function initParticles() {
+        particlesJS.load('particles-js', 'json/particles-polygon.json', function() {
+            console.log("This is Joy's blog page.");
+        });
+    }
+    function initNavClickEvent(){
+        $("#naviBar").click(function(e){
             var targetEl = e.target.parentNode;
             if (targetEl.id != "navi-right") {
                 if (!$(targetEl).hasClass("active")) {
@@ -24,11 +37,43 @@ window.onload = function() {
         }
     }
 
+    function initNavScrollEvent() {
+        var win = $(window);
+        win.scroll(function(){
+            var css = {};
+            var navBar = $("#naviBar");
+            if(win.scrollTop() > 0){
+                navBar.css({"background-color" : "#f8f8f8"});
+            }else{
+                navBar.css({"background-color" : "transparent"});
+            }
+            
+        });
+    }
+
+    function addListeners() {
+        initNavClickEvent();
+        initNavScrollEvent();
+
+    }
+
+    function initBgAnimation(css) {
+        css = {};
+        if (bgColorIdx >= colorList.length) {
+            bgColorIdx = 0;
+        }
+        css.backgroundColor = colorList[bgColorIdx];
+        bgColorIdx++;
+        setTimeout(function(){
+            $("#particles-js").animate(css, 5000, initBgAnimation(css));
+        },10000);
+    }
+
     function initEverything() {
-	    particlesJS.load('particles-js', 'json/particles-snow.json', function() {
-	    	console.log("This is Joy's blog page.");
-	    });
+        initGlobalVars();
+        initParticles();
         addListeners();
+        initBgAnimation();
         // initMainContent();
     }
 
